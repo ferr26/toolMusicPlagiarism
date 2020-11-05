b665bd3a3b24cf98aedba4cff5dfa3cfc30daeb8
Alfonso <a.delgaizo2@gmail.com>
2020-11-05 12:24:38 +0100

    titoli

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>Landing Page - Start Bootstrap Theme</title>
+  <title>MusicPlagiarismTool</title>
 
   <!-- Bootstrap core CSS -->
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -30,7 +30,7 @@
   <!-- Navigation -->
   <nav class="navbar navbar-light bg-light static-top">
     <div class="container">
-      <a class="navbar-brand" href="#">Music Plagiarism</a>
+      <a class="navbar-brand" href="#"></a>
     </div>
   </nav>
 
@@ -40,7 +40,7 @@
     <div class="container">
       <div class="row">
         <div class="col-xl-9 mx-auto">
-          <h1 class="mb-5">Build a landing page for your business or project and generate more leads!</h1>
+          <h1 class="mb-5">MusicPlagiarismTool</h1>
         </div>
         <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
         </div>
@@ -62,11 +62,11 @@
         <div class="file-upload" >
         <form action="upload.php" method="post" enctype="multipart/form-data">
 
-          <button id="btAggiungi" class="file-upload-btn-add" type="button" onclick="$('.file-upload-input').trigger( 'click' )">Aggiungi Spartito</button>
+          <button id="btAggiungi" class="file-upload-btn-add" type="button" onclick="$('.file-upload-input').trigger( 'click' )">Carica Spartiti</button>
           <div class="image-upload-wrap">
             <input class="file-upload-input" type='file' onchange="readURL(this);" accept=".mid,.mxl" multiple name="fileToUpload[]" />
             <div class="drag-text">
-              <h3>Drag and drop a file or select add Image</h3>
+              <h3>Trascina qui i tuoi spartiti</h3>
             </div>
           </div>
           <div class="file-upload-content">
@@ -77,7 +77,7 @@
             </div>
             <button class="file-upload-btn" type="button" onclick="removeUpload()">Elimina Spartiti</button>
          </div>
-         <input type="submit" value="Upload Image" name="submit">
+         <input type="submit" value="VERIFICA" name="submit">
         </form>
       </div>
 
@@ -130,8 +130,6 @@
           <div class="testimonial-item mx-auto mb-5 mb-lg-0">
             <img class="img-fluid rounded-circle mb-3" src="img/alfonso.jpg" alt="">
             <h5>Alfonso Del Gaizo</h5>
-            <i class="icon-trophy m-auto text-primary"></i>
-            <p class="font-weight-light mb-0">"This is fantastic! Thanks so much guys!"</p>
           </div>
         </div>
         <div class="col-lg-4">
@@ -143,47 +141,13 @@
           <div class="testimonial-item mx-auto mb-5 mb-lg-0">
             <img class="img-fluid rounded-circle mb-3" src="img/rosa1.jpg" alt="">
             <h5>Rosa Ferraioli</h5>
-            <p class="font-weight-light mb-0">"Thanks so much for making these free resources available to us!"</p>
           </div>
         </div>
       </div>
     </div>
   </section>
 
-   <!-- Icons Grid -->
-   <section class="features-icons bg-light text-center">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-4">
-          <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-            <div class="features-icons-icon d-flex">
-              <i class="icon-trophy m-auto text-primary"></i>
-            </div>
-            <h3>Fully Responsive</h3>
-            <p class="lead mb-0">This theme will look great on any device, no matter the size!</p>
-          </div>
-        </div>
-        <div class="col-lg-4">
-          <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-            <div class="features-icons-icon d-flex">
-              <i class="icon-layers m-auto text-primary"></i>
-            </div>
-            <h3>Bootstrap 4 Ready</h3>
-            <p class="lead mb-0">Featuring the latest build of the new Bootstrap 4 framework!</p>
-          </div>
-        </div>
-        <div class="col-lg-4">
-          <div class="features-icons-item mx-auto mb-0 mb-lg-3">
-            <div class="features-icons-icon d-flex">
-              <i class="icon-check m-auto text-primary"></i>
-            </div>
-            <h3>Easy to Use</h3>
-            <p class="lead mb-0">Ready to use with your own content, or customize the source files!</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+  
 
   <!-- Footer -->
   <footer class="footer bg-light">
@@ -200,13 +164,13 @@
               </a>
             </li>
             <li class="list-inline-item mr-3">
-              <a href="#">
-                <i class="fab fa-twitter-square fa-2x fa-fw"></i>
+              <a href="https://github.com/ferr26/toolMusicPlagiarism">
+                <i class="fab fa-github  fa-2x fa-fw"></i>
               </a>
             </li>
             <li class="list-inline-item">
-              <a href="#">
-                <i class="fab fa-instagram fa-2x fa-fw"></i>
+              <a href="https://www.unisa.it/">
+                <i class="fa fa-university fa-2x fa-fw"></i>
               </a>
             </li>
           </ul>
