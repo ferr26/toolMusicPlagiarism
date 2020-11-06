9ad4774c6deda68724821730b600284c604eeb11
ferr26 <r.ferraioli29@studenti.unisa.it>
2020-11-06 12:27:03 +0100

    upload2

---
+++ upload2.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>MusicPlagiarismTool</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="vendor2/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="css2/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body style="background-color:#e9e9e9;">
+
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-ligh static-top" style="background-color:#B0B0B0;">
+    <div class="container">
+    </div>
+  </nav>
+
+  <div class="container">
+
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
+
+      <div class="col-xl-10 col-lg-12 col-md-9">
+
+        <div class="card o-hidden border-0 shadow-lg my-5">
+          <div class="card-body p-0">
+            <!-- Nested Row within Card Body -->
+            <div class="row">
+              <!-- <div class="col-lg-6 d-none d-lg-block" style="background-color:#77dd77;">-->
+               <!--<div class="col-lg-6 d-none d-lg-block" style="background-color:#ff6961;"> -->
+              <div class="col-lg-6 d-none d-lg-block" style="background-color:#fdfd96;">
+              </div>
+              <div class="col-lg-6">
+                <div class="p-5">
+                  <div class="text-center">
+                    <h1 class="h1 text-gray-900 mb-4">PLAGIO</h1>
+                  </div>
+                  <div class="text-center">
+                    <h1 class="h1">90 (%)</h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+
+    <div class="text-center">
+         <a class="small" href="index.php">Torna all'Homepage</a>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 h-100 text-center text-lg-left my-auto">
+          <p class="text-muted small mb-4 mb-lg-0">&copy; Alfonso Del Gaizo - Rosa Ferraioli. All Rights Reserved.2020</p>
+        </div>
+        <div class="col-lg-6 h-100 text-center text-lg-right my-auto">
+          <ul class="list-inline mb-0">
+            <li class="list-inline-item mr-3">
+              <a href="#">
+                <i class="fa fa-git-square"></i>
+              </a>
+            </li>
+            <li class="list-inline-item mr-3">
+              <a href="https://github.com/ferr26/toolMusicPlagiarism">
+                <i class="fab fa-github  fa-2x fa-fw"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://www.unisa.it/">
+                <i class="fa fa-university fa-2x fa-fw"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+  </div>
+
+  
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html>
